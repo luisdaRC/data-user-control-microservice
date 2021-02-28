36902c0c8cae44db96a77b6592703c78f43effb9
luisdaRC <luixdani123@gmail.com>
2021-02-28 18:35:21 -0500

    Update endpoint in PhController finished

--- src/main/java/co/edu/unicartagena/control/infrastructure/controllers/PropiedadHorizontalController.java
+++ src/main/java/co/edu/unicartagena/control/infrastructure/controllers/PropiedadHorizontalController.java
@@ -72,10 +72,4 @@ public class PropiedadHorizontalController {
         return ResponseEntity.ok().body(updatePersonasYBienesCommand.ejecutar(personaListDTO));
     }
 
-    //Keep coding, starting by the next group of posts and gets from:
-    //https://github.com/luisdaRC/votations-frontend/blob/feature/revisor/src/app/services/sgph/propiedad-horizontal.service.ts
-
-    //Keep with the implementation of PersonalApoyo login endpoint. Taking into account jwt settings and so on (regarding to core).
-
-
 }
